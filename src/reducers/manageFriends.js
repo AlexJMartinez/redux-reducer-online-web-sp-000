ea22336b5896197145c3ad0c0fb1fa797c711070
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-06-24 18:06:19 -0700

    commit tree 318bf94338eccef5755aeb4576c6ad64ed8aa494

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,17 +1,20 @@
-export function manageFriends(state, action){
-    switch (action.type) {
+export function manageFriends(state={friends: []}, action){
+    switch (action.type) {    
         case 'ADD_FRIEND':
-          return {friends: state.friends + 1}
+            
+          return {...state, friends: [...state.friends, action.friend]}
         case 'REMOVE_FRIEND':
-          return {friends: state.friends - 1}
+            console.log(action)
+          return {...state, friends: [...state.friends.filter((friend) => friend.id !== action.id)] }
+
         default:
           return state;
     } 
 }
 
-let state = {
-    friends: []
-}
+// let state = {
+//     friends: []
+// }
 
 let increase = {
     type: "ADD_FRIEND",
